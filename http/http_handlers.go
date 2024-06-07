52ce86f6d5ccd620844ccc45f5c765576abc345b
Paul van Santen <pvansanten@transip.nl>
2024-06-07 15:30:14 +0200

    Allow single character ZFS filesystem names

--- http/http_handlers.go
+++ http/http_handlers.go
@@ -57,7 +57,7 @@ type SetProperties struct {
 }
 
 var (
-	validIdentifierRegexp  = regexp.MustCompile(`^[a-zA-Z0-9_]{2,100}$`)
+	validIdentifierRegexp  = regexp.MustCompile(`^[a-zA-Z0-9_]{1,100}$`)
 	validResumeTokenRegexp = regexp.MustCompile(`^[a-zA-Z0-9_-]{100,500}$`)
 )
 
